002fa74db46a5c40ceeec670dfe5334d8eaf4eac
iotatheta <princebiswas2006@gmail.com>
2026-04-20 21:26:38 +0530

    Update: find-all-numbers-disappeared-in-an-array - submission-0

--- Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-0.cpp
+++ Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-0.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) 
     {
-        vector<int> ans;
         int n=nums.size();
+        vector<int> ans;
         vector<int> freq(n+1,0);
         for(int i=0;i<n;i++)
         {
             freq[nums[i]]++;
-        }    
+        }
         for(int i=1;i<=n;i++)
         {
             if(freq[i]==0)
