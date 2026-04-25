a8c245393ed09543b4e0f2f5935964360d71890c
iotatheta <princebiswas2006@gmail.com>
2026-04-26 00:17:34 +0530

    Add: monotonic-array - submission-3

---
+++ Data Structures & Algorithms/monotonic-array/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) 
+    {
+        bool increase=true,decrease=true;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(!(nums[i]<=nums[i+1]))
+                increase=false;
+            if(!(nums[i]>=nums[i+1]))
+                decrease=false;
+        }    
+        return increase | decrease;
+    }
+};
