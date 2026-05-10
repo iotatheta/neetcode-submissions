6cabae305ac1e2017c464a6848d3df552a532d66
iotatheta <princebiswas2006@gmail.com>
2026-05-10 17:20:41 +0530

    Add: pascals-triangle - submission-2

---
+++ Data Structures & Algorithms/pascals-triangle/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) 
+    {
+        vector<vector<int>> ans(numRows);
+        // C(n,r) = C(n-1,r) + C(n-1,r-1)
+        for(int i=0;i<numRows;i++)
+        {
+            ans[i].resize(i+1,1);
+            ans[i][0] = ans[i][i] = 1;
+            for(int j=1;j<i;j++)
+            {
+                ans[i][j] = ans[i-1][j] + ans[i-1][j-1];
+            }
+        }
+        return ans;
+    }
+};
